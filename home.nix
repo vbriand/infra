585bea9da5834f30f7fb20b676e2571de10a7de4
Valentin Briand <678530+vbriand@users.noreply.github.com>
2025-11-23 15:41:02 +0100

    feat(zen-browser): set xdg mime associations
    Set Zen as the default application for various file types and URL
    schemes.

--- home.nix
+++ home.nix
@@ -189,6 +189,41 @@ in
         };
       };
     };
+  xdg = {
+    enable = true;
+    mimeApps =
+      let
+        value =
+          let
+            zen-browser = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.beta;
+          in
+          zen-browser.meta.desktopFileName;
+
+        associations = builtins.listToAttrs (
+          map
+            (name: {
+              inherit name value;
+            })
+            [
+              "application/x-extension-shtml"
+              "application/x-extension-xhtml"
+              "application/x-extension-html"
+              "application/x-extension-xht"
+              "application/x-extension-htm"
+              "x-scheme-handler/chrome"
+              "x-scheme-handler/about"
+              "x-scheme-handler/https"
+              "x-scheme-handler/http"
+              "application/xhtml+xml"
+              "text/html"
+            ]
+        );
+      in
+      {
+        associations.added = associations;
+        defaultApplications = associations;
+      };
+  };
   programs.zen-browser = {
     enable = true;
     policies =
