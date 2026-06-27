1190434856588cd9e5e19aeee975b34413256c36
Valentin Briand <678530+vbriand@users.noreply.github.com>
2026-06-27 17:26:06 +0200

    refactor(ludusavi): replace hardcoded paths with xdg variables

--- modules/ludusavi.nix
+++ modules/ludusavi.nix
@@ -4,21 +4,21 @@
       services.ludusavi.enable = true;
     };
 
-    provides.valou.homeManager = {
+    provides.valou.homeManager = { config, ... }: {
       services.ludusavi = {
         backupNotification = true;
         frequency = "*-*-* 22:00:00";
         settings = {
-          backup.path = "~/.local/state/backups/ludusavi";
+          backup.path = "${config.xdg.stateHome}/backups/ludusavi";
           language = "en-US";
-          restore.path = "~/.local/state/backups/ludusavi";
+          restore.path = "${config.xdg.stateHome}/backups/ludusavi";
           roots = [
             {
-              path = "~/.local/share/Steam";
+              path = "${config.xdg.dataHome}/Steam";
               store = "steam";
             }
             {
-              path = "~/.config/heroic";
+              path = "${config.xdg.configHome}/heroic";
               store = "heroic";
             }
             {
