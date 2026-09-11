c9fff0cc8c42eeb822392fad743869646658bf0d
Valentin Briand <678530+vbriand@users.noreply.github.com>
2026-09-11 02:15:46 +0200

    feat(steam-config): restore steam cloud functionality

--- modules/aspects/gaming/steam.nix
+++ modules/aspects/gaming/steam.nix
@@ -171,10 +171,13 @@
           };
           "3830" = {
             name = "Psychonauts";
+            compatTool = "Proton-GE";
+            # https://www.pcgamingwiki.com/wiki/Psychonauts#Steam_Cloud_not_working_(Linux)
+            env.XDG_DATA_HOME = "/mnt/games/SteamLibrary/steamapps/common";
             files.game = {
               remove = [
                 # Remove startup video
-                "WorkResource/cutscenes/prerendered/DFLogo.bik"
+                "WorkResource/Cutscenes/Prerendered/DFLogo.bik"
               ];
             };
           };
