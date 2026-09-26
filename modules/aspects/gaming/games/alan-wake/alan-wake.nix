ae55a0296266c5dd6f6b014ba7aff6bc6c99e08f
Valentin Briand <678530+vbriand@users.noreply.github.com>
2026-09-26 17:42:19 +0200

    feat(steam-config): add hd cutscenes for alan wake

--- modules/aspects/gaming/games/alan-wake/alan-wake.nix
+++ modules/aspects/gaming/games/alan-wake/alan-wake.nix
@@ -1,11 +1,42 @@
+{ inputs, ... }:
 {
-  den.aspects.steam.games.homeManager = {
+  flake-file.inputs = {
+    alan-wake-mods = {
+      url = "path:/mnt/games/mods/alan-wake";
+      flake = false;
+    };
+  };
+
+  den.aspects.steam.games.homeManager = { pkgs, ... }: {
     programs.steam.config.apps."108710" = {
       name = "Alan Wake";
       files.game = {
         place = {
           # Disable motion blur
           "shaders/build/pc".source = ./assets;
+          ".".source = pkgs.stdenv.mkDerivation {
+            pname = "alan-wake-remastered-cutscenes";
+            version = "1.0";
+            outputHash = "sha256-dv6A35g4+H5Lngruj9RUOj+q9q+st3XFIi82cPRH4sE=";
+            outputHashAlgo = "sha256";
+            outputHashMode = "nar";
+
+            # https://www.nexusmods.com/alanwake/mods/12
+            src = inputs.alan-wake-mods + "/remastered-cutscenes";
+
+            nativeBuildInputs = [ pkgs.unzip ];
+
+            dontBuild = true;
+            dontFixup = true;
+
+            unpackPhase = ''
+              runHook preUnpack
+              for zipfile in $src/*.zip; do
+                unzip -q "$zipfile" -d "$out"
+              done
+              runHook postUnpack
+            '';
+          };
         };
         remove = [
           # Remove startup videos
