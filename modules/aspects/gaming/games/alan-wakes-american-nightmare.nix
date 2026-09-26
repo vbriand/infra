f3e00bbd1ab2e9bf057a891730299245e2035bb3
Valentin Briand <678530+vbriand@users.noreply.github.com>
2026-09-26 17:45:03 +0200

    feat(steam-config): add hd cutscenes for alan wake's american nightmare

--- modules/aspects/gaming/games/alan-wakes-american-nightmare.nix
+++ modules/aspects/gaming/games/alan-wakes-american-nightmare.nix
@@ -1,8 +1,41 @@
+{ inputs, ... }:
 {
-  den.aspects.steam.games.homeManager = {
+  flake-file.inputs = {
+    alan-wakes-american-nightmare-mods = {
+      url = "path:/mnt/games/mods/alan-wakes-american-nightmare";
+      flake = false;
+    };
+  };
+
+  den.aspects.steam.games.homeManager = { pkgs, ... }: {
     programs.steam.config.apps."202750" = {
       name = "Alan Wake's American Nightmare";
       files.game = {
+        place = {
+          ".".source = pkgs.stdenv.mkDerivation {
+            pname = "alan-wakes-american-nightmare-upscaled-cinematics";
+            version = "1.0";
+            outputHash = "sha256-nkhf+a2r8xyOKoIhhuAlKxbgOa+6uLBYs9r/3ODq6Io=";
+            outputHashAlgo = "sha256";
+            outputHashMode = "nar";
+
+            # https://www.nexusmods.com/alanwakesamericannightmare/mods/6
+            src =
+              inputs.alan-wakes-american-nightmare-mods
+              + "/Upscaled Cinematics (HEAVY Version)-6-1-00-1738783601.zip";
+
+            nativeBuildInputs = [ pkgs.unzip ];
+
+            dontBuild = true;
+            dontFixup = true;
+
+            unpackPhase = ''
+              runHook preUnpack
+              unzip -q "$src" -d "$out"
+              runHook postUnpack
+            '';
+          };
+        };
         remove = [
           # Remove startup video
           "data/videos/startup_remedy.bik"
